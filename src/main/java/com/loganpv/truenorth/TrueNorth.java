3b55f55c83a9c75324e3a08607aebc9f9d29d837
Logan <loganvanmc@outlook.com>
2026-06-11 09:33:51 -0500

    Cleaned Unnecessary Objects (Removed Bloat) - Logan

--- src/main/java/com/loganpv/truenorth/TrueNorth.java
+++ src/main/java/com/loganpv/truenorth/TrueNorth.java
@@ -1,16 +1,9 @@
 package com.loganpv.truenorth;
 
 import com.loganpv.truenorth.item.RegisterItem;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(TrueNorth.MODID)
 public class TrueNorth {
@@ -19,29 +12,6 @@ public class TrueNorth {
 
     public TrueNorth(IEventBus modEventBus, ModContainer modContainer)
     {
-        modEventBus.addListener(this::commonSetup);
-
-        NeoForge.EVENT_BUS.register(this);
-
         RegisterItem.register(modEventBus);
-    }
-
-    private void commonSetup(FMLCommonSetupEvent event) {
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-
-    }
-
-    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
-
-        }
     }
 }
