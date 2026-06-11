1cb748350b25af1f1e31f3c12a77638c720894a8
Logan <loganvanmc@outlook.com>
2026-06-11 10:21:00 -0500

    Added Magnetic Compass to Tools and Utilities in the Creative Menu. - Logan

--- src/main/java/com/loganpv/truenorth/TrueNorth.java
+++ src/main/java/com/loganpv/truenorth/TrueNorth.java
@@ -1,11 +1,17 @@
 package com.loganpv.truenorth;
 
 import com.loganpv.truenorth.item.RegisterItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(TrueNorth.MODID)
+@EventBusSubscriber(modid = TrueNorth.MODID, value = Dist.CLIENT)
 public class TrueNorth {
 
     public static final String MODID = "truenorth";
@@ -13,5 +19,12 @@ public class TrueNorth {
     public TrueNorth(IEventBus modEventBus, ModContainer modContainer)
     {
         RegisterItem.register(modEventBus);
+    }
+
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            event.accept(RegisterItem.MAGNETICCOMPASS.get());
+        }
     }
 }
