8da9ed30bd44a05874e7535df61509f8c3c76d96
Logan <loganvanmc@outlook.com>
2026-06-16 20:30:09 -0500

    Added Smoothing to Compass Movement

--- src/main/java/com/loganpv/truenorth/TrueNorthClient.java
+++ src/main/java/com/loganpv/truenorth/TrueNorthClient.java
@@ -4,6 +4,7 @@ import com.loganpv.truenorth.item.MagneticCompass;
 import com.loganpv.truenorth.item.RegisterItem;
 import net.minecraft.client.renderer.item.ItemProperties;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -11,12 +12,15 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 
 @EventBusSubscriber(modid = TrueNorth.MODID, value = Dist.CLIENT)
 public class TrueNorthClient {
+    static float current;
+
     @SubscribeEvent
     public static void clientInit(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             ItemProperties.register(RegisterItem.MAGNETIC_COMPASS.get(), ResourceLocation.fromNamespaceAndPath(TrueNorth.MODID, "angle"), (stack, world, player, seed) -> {
                 if (player != null) {
-                    return MagneticCompass.GetYRot(player);
+                    current = Mth.positiveModulo(Mth.rotLerp(0.05f, current, player.getVisualRotationYInDegrees() + 180), 360);
+                    return current / 360;
                 }
                 else {
                     return 0f;
